c5f37b12cc997091f1387134cffb2046a86b9c5f
birame <birametgod95@gmail.com>
2019-02-17 11:34:58 +0100

    feat(errorHandler) : manage error handler

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -1,14 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { UserService } from '../user.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
   styleUrls: ['./signup.component.css']
 })
-export class SignupComponent implements OnInit {
+export class SignupComponent implements OnInit, OnDestroy {
   form: FormGroup;
+  isLoading = false;
+  private subs: Subscription;
   constructor(private userService: UserService) {}
 
   ngOnInit() {
@@ -16,12 +19,21 @@ export class SignupComponent implements OnInit {
       email: new FormControl(null, { validators: [Validators.required, Validators.email] }),
       password: new FormControl(null, { validators: [Validators.required, Validators.minLength(6)] })
     });
+    this.subs = this.userService.getUserAuthenticateListener().subscribe(value => {
+      this.isLoading = false;
+    });
   }
 
   onSignUp() {
     if (this.form.invalid) {
       return;
     }
+    this.isLoading = true;
     this.userService.addUser(this.form.value.email, this.form.value.password);
+    this.form.reset();
+  }
+
+  ngOnDestroy() {
+    this.subs.unsubscribe();
   }
 }
