243b04fe0349c388ffd4828c9d7f85782ac9497e
birame <birametgod95@gmail.com>
2019-02-27 01:51:58 +0100

    feat(integrationTest): test get posts

---
+++ backend/tests/models/posts.test.js
@@ -0,0 +1,28 @@
+const app = require('../../app');
+const request = require('supertest');
+const mongoose = require('mongoose');
+
+describe('test the posts path', () => {
+  beforeEach(() => {
+    mongoose
+      .connect('mongodb+srv://birametgod:ID6nlTZu07PYxCbd@cluster0-c7tiq.mongodb.net/node-angular_tests', {
+        useNewUrlParser: true
+      })
+      .then(() => {
+        console.log('connected to database');
+      })
+      .catch(() => {
+        console.log('Connection failed !');
+      });
+  });
+  afterAll(done => {
+    mongoose.disconnect(done);
+  });
+
+  describe('GET /', () => {
+    it('should return all posts', async () => {
+      const res = await request(app).get('/api/posts');
+      expect(res.status).toBe(200);
+    });
+  });
+});
