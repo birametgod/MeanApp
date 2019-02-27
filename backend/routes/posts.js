46b98a9fa325c9fa7652d0a9f2784a1ca79d6d47
birame <birametgod95@gmail.com>
2019-02-27 01:03:43 +0100

    feat(Optimization): using controllers

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-const Post = require('../models/post');
 const multer = require('multer');
 const MIME_TYPE_MAP = {
   'image/png': 'png',
@@ -8,6 +7,8 @@ const MIME_TYPE_MAP = {
   'image/jpg': 'jpg'
 };
 const checkAuth = require('../middleware/check-auth');
+const postController = require('../controllers/post');
+
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     const isValid = MIME_TYPE_MAP[file.mimetype];
@@ -27,122 +28,14 @@ const storage = multer.diskStorage({
   }
 });
 
-router.post('', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
-  const url = req.protocol + '://' + req.get('host');
-  const post = new Post({
-    name: req.body.name,
-    post: req.body.post,
-    imagePath: url + '/images/' + req.file.filename,
-    creator: req.userData.userId
-  });
-  post.save((err, result) => {
-    if (err) {
-      return res.status(500).json({
-        message: 'failed',
-        error: err
-      });
-    }
-    return res.status(201).json({
-      message: 'post added successfully',
-      post: {
-        ...result,
-        id: result._id
-      }
-    });
-  });
-});
+router.post('', checkAuth, multer({ storage: storage }).single('image'), postController.createPost);
 
-router.get('/:id', (req, res, next) => {
-  Post.findById(req.params.id, (err, result) => {
-    if (err) {
-      res.status(500).json({
-        message: ' not found '
-      });
-    }
-    if (result) {
-      res.status(200).json(result);
-    } else {
-      res.status(500).json({
-        message: ' not found '
-      });
-    }
-  });
-});
+router.get('/:id', postController.readOnePost);
 
-router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), (req, res, next) => {
-  let imagePath = req.body.imagePath;
-  if (req.file) {
-    const url = req.protocol + '://' + req.get('host');
-    imagePath = url + '/images/' + req.file.filename;
-  }
-  const post = new Post({
-    _id: req.body.id,
-    name: req.body.name,
-    post: req.body.post,
-    imagePath: imagePath,
-    creator: req.userData.userId
-  });
-  Post.updateOne({ _id: req.params.id, creator: req.userData.userId }, post, (err, result) => {
-    if (err) {
-      return res.status(404).json({
-        error: err
-      });
-    }
-    if (result.nModified <= 0) {
-      return res.status(401).json({
-        message: 'update failed'
-      });
-    }
-    return res.status(200).json({
-      message: 'update successfully'
-    });
-  });
-});
+router.put('/:id', checkAuth, multer({ storage: storage }).single('image'), postController.updatePost);
 
-router.get('', (req, res, next) => {
-  const currentPage = +req.query.page;
-  const size = +req.query.size;
-  const postQuery = Post.find();
-  let fetchedPosts;
-  if (currentPage && size) {
-    postQuery.skip(size * (currentPage - 1)).limit(size);
-  }
-  postQuery
-    .find()
-    .then(docs => {
-      fetchedPosts = docs;
-      return Post.countDocuments();
-    })
-    .then(count => {
-      res.status(200).json({
-        message: 'successful',
-        post: fetchedPosts,
-        maxPost: count
-      });
-    })
-    .catch(err => {
-      res.status(500).json({
-        message: 'failed',
-        err: err
-      });
-    });
-});
+router.get('', postController.readPost);
 
-router.delete('/:id', checkAuth, (req, res, next) => {
-  Post.deleteOne({ _id: req.params.id, creator: req.userData.userId }, (err, result) => {
-    if (err) {
-      return res.status(500).json({
-        message: 'delete failed',
-        err: err
-      });
-    }
-    if (result.n <= 0) {
-      return res.status(401).json({
-        message: 'delete failed , not Authorized'
-      });
-    }
-    res.status(200).json({ message: 'Post deleted' });
-  });
-});
+router.delete('/:id', checkAuth, postController.deletePost);
 
 module.exports = router;
